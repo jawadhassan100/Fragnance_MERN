c4e01ec10b3c8483c1cd7a5df813b5386595dfc5
jawadhassan100 <jawadhassankhan2001@gmail.com>
2025-02-11 21:56:39 +0500

    bug fixed

--- Backend/controller/cartController.js
+++ Backend/controller/cartController.js
@@ -1,5 +1,4 @@
-const Cart = require('../model/Cart'); 
-
+const Cart = require("../model/Cart");
 
 // Add item to Cart
 exports.addToCart = async (req, res) => {
@@ -9,29 +8,28 @@ exports.addToCart = async (req, res) => {
     let cart = await Cart.findOne({ sessionId });
 
     if (cart) {
-      const productIndex = cart.items.findIndex(item => item.product.toString() === productId);
+      const productIndex = cart.items.findIndex(
+        (item) => item.product.toString() === productId
+      );
 
       if (productIndex >= 0) {
-    
         cart.items[productIndex].quantity += quantity;
       } else {
-       
         cart.items.push({ product: productId, quantity });
       }
 
       cart.lastModified = new Date();
       await cart.save();
     } else {
-      
       cart = new Cart({
         sessionId,
-        items: [{ product: productId, quantity }]
+        items: [{ product: productId, quantity }],
       });
 
       await cart.save();
     }
 
-    res.status(200).json({ message: 'Product added to cart', cart });
+    res.status(200).json({ message: "Product added to cart", cart });
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
@@ -45,19 +43,21 @@ exports.editCartItem = async (req, res) => {
     const cart = await Cart.findOne({ sessionId });
 
     if (!cart) {
-      return res.status(404).json({ error: 'Cart not found' });
+      return res.status(404).json({ error: "Cart not found" });
     }
 
-    const productIndex = cart.items.findIndex(item => item.product.toString() === productId);
+    const productIndex = cart.items.findIndex(
+      (item) => item.product.toString() === productId
+    );
 
     if (productIndex < 0) {
-      return res.status(404).json({ error: 'Product not found in the cart' });
+      return res.status(404).json({ error: "Product not found in the cart" });
     }
 
     cart.items[productIndex].quantity = quantity;
     cart.lastModified = new Date();
     await cart.save();
-    res.status(200).json({ message: 'Product quantity updated', cart });
+    res.status(200).json({ message: "Product quantity updated", cart });
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
@@ -72,17 +72,17 @@ exports.removeFromCart = async (req, res) => {
     const cart = await Cart.findOne({ sessionId });
 
     if (!cart) {
-      return res.status(404).json({ error: 'Cart not found' });
+      return res.status(404).json({ error: "Cart not found" });
     }
 
     // Remove the product from the cart
-    cart.items = cart.items.filter(item => item.product.toString() !== productId);
+    cart.items = cart.items.filter(
+      (item) => item.product.toString() !== productId
+    );
     cart.lastModified = new Date();
     await cart.save();
-    res.status(200).json({ message: 'Product removed from cart', cart });
+    res.status(200).json({ message: "Product removed from cart", cart });
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
 };
-
-
