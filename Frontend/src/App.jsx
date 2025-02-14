7a0621c599666602c42a483658bc86dad4d12060
jawadhassan100 <jawadhassankhan2001@gmail.com>
2025-02-14 15:16:08 +0500

    aboutus navigation

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import { Routes, Route } from "react-router-dom";
 import HomePage from './HomePage';
 import Navbar from './components/Navbar/Navbar'
+import AboutUs from './components/AboutUs/AboutUs'
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <Navbar/>
       <Routes>
         <Route path='/' element={<HomePage/>}/>
+        <Route path='/about-us' element={<AboutUs/>}/>
       </Routes>
     </>
   )
