5324605355aa8f12b0bdd23d24d5983dd0998b55
jawadhassan100 <jawadhassankhan2001@gmail.com>
2025-02-15 22:31:47 +0500

    why choose us added

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import HomePage from './HomePage';
 import Navbar from './components/Navbar/Navbar'
 import AboutUs from './components/AboutUs/AboutUs'
+import WhyChooseUs from './components/WhyChooseUs/WhyChooseUs';
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
       <Routes>
         <Route path='/' element={<HomePage/>}/>
         <Route path='/about-us' element={<AboutUs/>}/>
+        <Route path='/whychooseus' element={<WhyChooseUs/>}/>
       </Routes>
     </>
   )
